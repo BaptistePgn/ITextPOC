6817054821d17ffb5fb44e472a86b5e7c82e4fb1
Baptiste PEGNE <b.pegne@auditpl.fr>
2024-04-05 17:03:00 +0200

    feat: added first example of pdf generation and complete readme

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using System.Data;
+
+using var document = new Document(new PdfDocument(new PdfWriter("invoice.pdf")));
+
+// Add the header
+Paragraph headerLeft = new Paragraph("Name \n " +
+       "Siren Number \n " +
+       "Head office address \n " +
+       "Type of company \n " +
+       "Amount of share capital \n" +
+       "VAT identification number")
+    .SetTextAlignment(TextAlignment.LEFT);
+
+// Add first left top paragraph
+document.Add(headerLeft);
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# ITextSharpPOC
+# ITextPOC
+
+This is a POC for testing the IText library for PDF generation.
+
+source: https://github.com/itext/itext-dotnet
+
+## Framework used
+	- .Net8
+
+## Packages used 
+    - itext7 (8.0.3)
+    - itext7.bouncy-castle-adapter(8.0.3)
+
+## How to use
+
+1. Clone the repository
+2. Open the solution in Visual Studio
+3. Run the project
+
